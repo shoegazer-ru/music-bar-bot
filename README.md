8d33d4d3d0077d947521ea7472f5ff6d11cba8d3
Sergey Golubev <noirinmybox@gmail.com>
2021-03-17 09:02:15 +0500

    Убрали конфиг, сделали шаблон

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # music-bar-bot
 discord bot for music bar server
+create config.json from config.json.template
--- index.js
+++ index.js
@@ -1,90 +1,96 @@
 const {Client, MessageEmbed} = require("discord.js");
 const config = require("./config.json");
 
-clientLog('start bot service');
+App.start();
 
-const client = new Client();
+const App = {
+    client: null,
 
-client.on("ready", function(){
-    clientLog(`the client becomes ready to start`);
-    clientLog(`I am ready! Logged in as ${client.user.tag}!`);
-});
+    start: function(){
+        App.log('start bot service');
 
-client.on("guildMemberUpdate", function(oldMember, newMember){
-    clientLog(`a guild member updated`);
-    handleWelcomeRoleAdd(oldMember, newMember);
-});
+        this.client = new Client();
 
-client.on("userUpdate", function(oldMember, newMember){
-    clientLog(`a user updated`);
-    handleWelcomeRoleAdd(oldMember, newMember);
-});
+        this.client.on("ready", function(){
+            App.log(`the this.client becomes ready to start`);
+            App.log(`I am ready! Logged in as ${this.client.user.tag}!`);
+        });
 
-client.on("presenceUpdate", function(oldMember, newMember){
-    clientLog('a precense updated');
-    handleWelcomeRoleAdd(oldMember, newMember);
-});
+        this.client.on("guildMemberUpdate", function(oldMember, newMember){
+            App.log(`a guild member updated`);
+            App.handleWelcomeRoleAdd(oldMember, newMember);
+        });
 
-client.login(config.bot_token);
+        this.client.on("userUpdate", function(oldMember, newMember){
+            App.log(`a user updated`);
+            App.handleWelcomeRoleAdd(oldMember, newMember);
+        });
 
-function clientLog(message, data) {
-    if (!data) {
-        console.log(message);
-    } else {
-        console.log(message, data);
-    }
-}
+        this.client.on("presenceUpdate", function(oldMember, newMember){
+            App.log('a precense updated');
+            App.handleWelcomeRoleAdd(oldMember, newMember);
+        });
 
-function handleWelcomeRoleAdd(oldMember, newMember) {
-    let newRoles = getNewRoles(oldMember, newMember);
-    if (!newRoles || !newRoles.length) {
-        return null;
-    }
+        this.client.login(config.bot_token);
+    },
 
-    newRoles.map(function(roleName){
-        if (roleName == config.welcome_role) {
-            postWelcomeMessage(newMember);
+    log: function(message, data){
+        if (!data) {
+            console.log(message);
+        } else {
+            console.log(message, data);
         }
-    })
-}
+    },
 
-function postWelcomeMessage(member) {
-    let username = member.toString(); // member.nickname || member.user.username || member.user.id;
-    if (!username) {
-        clientLog('fail to get member username');
-        return null;
-    }
-
-    const channel = client.channels.cache.get(config.welcome_message_channel);
-    if (!channel) {
-        clientLog('welcome message channel not found');
-        return null;
-    }
-
-    const embed = new MessageEmbed(); 
-    embed.setTitle(config.welcome_message_title); 
-    embed.setDescription(config.welcome_message_body.replace('{username}', username));
-    channel.send(embed); 
-
-    // channel.send(config.welcome_message.replace('{username}', username));
-}
-
-function getNewRoles(oldMember, newMember) {
-    let oldMemberRoles = [],
-        newMemberRoles = [];
-    oldMember.roles.cache.map(function(role){
-        oldMemberRoles.push(role.name);
-    });
-    newMember.roles.cache.map(function(role){
-        newMemberRoles.push(role.name);
-    });
-
-    let newRoles = [];
-    newMemberRoles.map(function(roleName){
-        if (oldMemberRoles.indexOf(roleName) <= -1) {
-            newRoles.push(roleName);
+    handleWelcomeRoleAdd: function(oldMember, newMember){
+        let newRoles = getNewRoles(oldMember, newMember);
+        if (!newRoles || !newRoles.length) {
+            return null;
         }
-    });
 
-    return newRoles;
-}
+        newRoles.map(function(roleName){
+            if (roleName == config.welcome_role) {
+                App.postWelcomeMessage(newMember);
+            }
+        })
+    },
+
+    postWelcomeMessage: function(member) {
+        let username = member.toString(); // member.nickname || member.user.username || member.user.id;
+        if (!username) {
+            App.log('fail to get member username');
+            return null;
+        }
+
+        const channel = App.client.channels.cache.get(config.welcome_message_channel);
+        if (!channel) {
+            App.log('welcome message channel not found');
+            return null;
+        }
+
+        const embed = new MessageEmbed(); 
+        embed.setTitle(config.welcome_message_title); 
+        embed.setDescription(config.welcome_message_body.replace('{username}', username));
+        channel.send(embed); 
+    },
+
+    getNewRoles: function(oldMember, newMember) {
+        let oldMemberRoles = [],
+            newMemberRoles = [];
+        oldMember.roles.cache.map(function(role){
+            oldMemberRoles.push(role.name);
+        });
+        newMember.roles.cache.map(function(role){
+            newMemberRoles.push(role.name);
+        });
+
+        let newRoles = [];
+        newMemberRoles.map(function(roleName){
+            if (oldMemberRoles.indexOf(roleName) <= -1) {
+                newRoles.push(roleName);
+            }
+        });
+
+        return newRoles;
+    }
+};
